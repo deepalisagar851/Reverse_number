f114c92aa374f11f25d0f437d361e30e8545712b
Deepali <96642348+deepalisagar851@users.noreply.github.com>
2022-04-19 14:27:07 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Reverse_number
+{
+    
+        class Program
+        {
+            static void Main(string[] args)
+            {
+              
+                int Num, Rem;
+            int Reverse=0;
+                Console.WriteLine("Enter a Number :");
+                Num =int.Parse(Console.ReadLine());
+                while (Num != 0)
+                {
+                    Rem = Num%10;
+                Reverse = Reverse * 10 + Rem;
+                Num /= 10;
+
+                }
+                Console.WriteLine("Reverese Number is :{0}", Reverse);
+                Console.ReadLine();
+
+
+            }
+        }
+    
+
+
+}
